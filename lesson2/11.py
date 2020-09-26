c849c1a819ca4fc9c90037f268f6a60dbd9ae116
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-09-26 16:48:10 +0600

    sdsdsd'

--- lesson2/11.py
+++ lesson2/11.py
@@ -1,6 +1,9 @@
 a = input("Введите сумму займа:")
 b = input("Введите процент:")
 money = int(a)
-percent = int(b)/100
-total = money+(money*percent)
-print(total)
+percent = int(b)
+percent_money = (money * percent) / 100
+total = money + percent_money
+s = f"Вы должны оплатить {total} при том что {money} долг  а {percent_money} вышла как процент"
+print(s)
+#Вы должны оплатить 275 при том что 250 долг  а 25 вышла как процент
