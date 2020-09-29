22e5dbd967fd67b08d78e70297754f31b3c73ca4
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-09-29 20:52:10 +0600

    sdsdd

---
+++ lesson3/6.py
@@ -0,0 +1,13 @@
+# в консоль пишется ваше имя если ваше имя admin то
+# вывести добро пожаловать а если нет то вывести
+# ошибка
+name = input()
+if name == "admin":
+    print("welcome")
+else:
+    print("error")
+
+# if name == "admin":
+#     print("welcome")
+# if name != "admin":
+#     print("error")
