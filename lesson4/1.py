6cec77c5c2da450226e1f0c03157663c83976296
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-01 19:16:04 +0600

    sdsd

--- lesson4/1.py
+++ lesson4/1.py
@@ -8,3 +8,28 @@
 # ВЫВОД:
 # Максимальное число:5
 # Минимальное число:3
+a = int(input())
+b = int(input())
+c = int(input())
+maxi = 0
+mini = 0
+#find maximum
+if a > b and a > c:
+    maxi = a
+
+if b > a and b > c:
+    maxi = b
+
+if c > a and c > b:
+    maxi = c
+#find minimum
+if a < b and a < c:
+    mini = a
+
+if b < a and b < c:
+    mini = b
+
+if c < a and c < b:
+    mini = c
+print(f"Максимальное число {maxi}")
+print(f"Минимальное число {mini}")
