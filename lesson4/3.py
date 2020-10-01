cdcda373c6c69f1e24ee74b84aecbfbfef9bcc85
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-01 19:28:45 +0600

    sdsd

--- lesson4/3.py
+++ lesson4/3.py
@@ -10,3 +10,37 @@
 # 3
 # 0
 # среднее число:3
+a = int(input())
+b = int(input())
+c = int(input())
+maxi = 0
+mini = 0
+middle = 0
+#find maximum
+if a > b and a > c:
+    maxi = a
+
+if b > a and b > c:
+    maxi = b
+
+if c > a and c > b:
+    maxi = c
+#find minimum
+if a < b and a < c:
+    mini = a
+
+if b < a and b < c:
+    mini = b
+
+if c < a and c < b:
+    mini = c
+#find middle
+if a != maxi and a != mini:
+    middle = a
+if b != maxi and b != mini:
+    middle = b
+if c != maxi and c != mini:
+    middle = c
+print(f"Максимальное число {maxi}")
+print(f"Минимальное число {mini}")
+print(f"Среднее число {middle}")
