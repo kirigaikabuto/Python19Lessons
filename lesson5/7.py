0e8d8329de3017a681a78cae00576949abf4ef07
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-03 16:28:19 +0600

    sdsd

--- lesson5/7.py
+++ lesson5/7.py
@@ -1,5 +1,60 @@
 #5
 #15
+n = int(input())
+i = 0
+sumi = 0
+while i <= n:
+    sumi = sumi + i
+    i += 1
+print(sumi)
+# i = 0
+# sumi = 0 + 0 =0
+# i = 1
+# sumi = 0 + 1 = 1
+# i = 2
+# sumi = 1 + 2 = 3
+# i =3
+# sumi = 3 + 3 = 6
+# i = 4
+# sumi = 6 + 4 = 10
 
 
-1 + 2 +3 + 4 + 5 = 15
+# 1 + 2 +3 + 4 + 5 = 15
+
+# Excell
+# Name     Username  Password   money
+# Azoria   Azorias   sdsdsdsds  100000
+# Куаныш   Куаныш    фывфвфв    100000
+# Yerassyl Yerassyl  asdsad     100000
+#
+# [
+#     {
+#         "name":"azoria",
+#         "username":"azoria",
+#         "password":"asdsdsdsd",
+#         "money":100000
+#     },
+# {
+#         "name":"azoria",
+#         "username":"azoria",
+#         "password":"asdsdsdsd",
+#         "money":100000
+#     }
+# {
+#         "name":"azoria",
+#         "username":"azoria",
+#         "password":"asdsdsdsd",
+#         "money":100000
+#     },{
+#         "name":"azoria",
+#         "username":"azoria",
+#         "password":"asdsdsdsd",
+#         "money":100000
+#     }
+# {
+#         "name":"azoria",
+#         "username":"azoria",
+#         "password":"asdsdsdsd",
+#         "money":100000
+#     }
+# ]
