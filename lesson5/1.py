b26aab1eaed39f5da049efb526daa3dc23212377
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-03 15:26:08 +0600

    sdsd

---
+++ lesson5/1.py
@@ -0,0 +1,10 @@
+#while -> пока(до тех пор)
+# while условие:
+#     действия
+
+number = 20
+while number != 10:
+    print("ok", number)
+    number = number - 1
+
+print("end of cycle")
---
+++ lesson5/2.py
@@ -0,0 +1,8 @@
+balance = 200
+year = 5
+year_cache = 10
+i = 0
+while i < 5:
+    balance = balance + year_cache
+    i += 1
+print(f"Total balance:{balance}")
