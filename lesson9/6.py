8d0de972ef030a09070df7a93996aecd96436c9f
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-17 16:06:31 +0600

    sdsd

--- lesson9/6.py
+++ lesson9/6.py
@@ -1,5 +1,22 @@
-file = open("clothes.txt", "r")  # r ->read
-data = file.read()
-clothes = data.split("\n")
-#code
-
+# clothes
+file1 = open("clothes.txt", "r")  # r ->read
+data1 = file1.read()
+clothes = data1.split("\n")
+n = len(clothes)
+# prices
+file2 = open("prices.txt", "r")
+data2 = file2.read()
+prices_str = data2.split("\n")
+i = 0
+prices = []
+while i < n:
+    num = int(prices_str[i])
+    prices.append(num)
+    i += 1
+#main
+i = 0
+while i < n:
+    print(f"{i}.{clothes[i]}")
+    i += 1
+choice = int(input("выбрать:"))
+print(clothes[choice], prices[choice])
