f4074f152eb3caafcc3390725b846bc5f940b46d
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-22 20:39:15 +0600

    sdsd

--- lesson10/10.py
+++ lesson10/10.py
@@ -2,11 +2,17 @@ clothes = []
 file = open("database.txt", 'r')
 data = file.read()
 clothesArrStr = data.split("\n")
+#clothesArrStr->['футболка,20', 'куртка,30', 'плащ,40', 'кимано,30', 'sdsd,123']
 name = input("name:")
 price = input("price:")
 product = name + "," + price
 clothesArrStr.append(product)
 s = "\n".join(clothesArrStr)
+# 'футболка,20'
+# 'куртка,30'
+# 'плащ,40'
+# 'кимано,30'
+# 'sdsd,123'
 newFile = open("database.txt", "w")
 newFile.write(s)
 newFile.close()
