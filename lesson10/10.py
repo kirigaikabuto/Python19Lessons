539993f0a9052882107ed2c1a2325b7e513857b0
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-10-22 20:37:28 +0600

    sdsd

--- lesson10/10.py
+++ lesson10/10.py
@@ -2,17 +2,21 @@ clothes = []
 file = open("database.txt", 'r')
 data = file.read()
 clothesArrStr = data.split("\n")
-n = len(clothesArrStr)
-i = 0
-while i < n:
-    clothesPart = clothesArrStr[i].split(",")  # ["куртка","20"]
-    clothes.append(clothesPart)
-    i += 1
-n = len(clothes)
-i = 0
-while i < n:
-    print(clothes[i][0], clothes[i][1])
-    i += 1
+name = input("name:")
+price = input("price:")
+product = name + "," + price
+clothesArrStr.append(product)
+s = "\n".join(clothesArrStr)
+newFile = open("database.txt", "w")
+newFile.write(s)
+newFile.close()
+
+
+# n = len(clothes)
+# i = 0
+# while i < n:
+#     print(clothes[i][0], clothes[i][1])
+#     i += 1
 
 # кимано
 # 30
