bb51fe10c620b49bdf69ff4f730f5f2a5d5af4c3
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-03 19:36:58 +0600

    sdsd

--- lesson13/2.py
+++ lesson13/2.py
@@ -3,15 +3,17 @@ import random
 arr = []
 n = int(input("count of element in array:"))
 for i in range(n):
-    k = random.randint(10, 20)
+    k = random.randint(1, 100)
     arr.append(k)
 
 #
-# 3
+# 5
 # [
-#     [1,0,-1],
-#     [34,12,23],
-#     [43,123,32],
+#     [1,0,-1,12,3],
+#     [34,12,23,23,1],
+#     [43,123,32,3,4],
+#     [43,123,32,5,6],
+#      [43,123,32,5,],
 # ]
 # i = 0
 # while i < n:
