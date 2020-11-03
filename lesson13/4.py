13a17d0ab34fca22bbe594d55d060534d0d24554
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-03 20:18:09 +0600

    sdsd

--- lesson13/4.py
+++ lesson13/4.py
@@ -1,5 +1,20 @@
 s = "1 2 3,4 5 6,7 8 9"
-#code
+parts = s.split(",")
+all = []
+for i in parts:
+    numbers = i.split(" ")#['1','2','3']
+    temp = []
+    for j in numbers:
+        temp.append(int(j))#[1,2,3]
+    all.append(temp)
+
+for i in all:
+    print(i)
+# parts = [
+#  '1 2 3',
+#  '4 5 6',
+#  '7 8 9'
+# ]
 
 # [
 #     [1,2,3],
