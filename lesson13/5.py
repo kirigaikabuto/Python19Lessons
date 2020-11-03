00c254f9c9e80d10434c11045cd8803239fa0e2c
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-03 20:39:04 +0600

    sdsd

--- lesson13/5.py
+++ lesson13/5.py
@@ -6,9 +6,6 @@ for i in range(n):
         arr.append(0)
     all.append(arr)
 
-for i in all:
-    print(i)
-
 points = [
     [0, 0],
     [3, 4],
@@ -16,8 +13,13 @@ points = [
     [1, 3]
 ]
 
-[1, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 1]
-[0, 0, 0, 1, 0]
+for point in points:
+    x, y = point  # [3,4] -> x=3,y=4
+    all[x][y] = 1
+for i in all:
+    print(i)
+# [1, 0, 0, 0, 0]
+# [0, 0, 0, 0, 0]
+# [0, 0, 0, 0, 0]
+# [0, 0, 0, 0, 1]
+# [0, 0, 0, 1, 0]
