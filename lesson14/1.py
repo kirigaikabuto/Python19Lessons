120339934841b04ffe3000f81047595eb9d91ec4
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-05 19:27:04 +0600

    sdsd

---
+++ lesson14/1.py
@@ -0,0 +1,15 @@
+person1 = {
+    "first_name": "yerassyl",
+    "last_name": "tleugazy",
+    "age": 22,
+}
+person2 = {
+    "first_name": "person2",
+    "last_name": "person2",
+    "age": 50,
+}
+print(person1["first_name"], person1["last_name"], person1["age"])
+print(person2["first_name"], person2["last_name"], person2["age"])
+#full_name -> tleugazy erassyl
+#age -> 22
+#salary -> 1000
--- lesson14/main.py
+++ lesson14/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+# Телефонная книжка
+# имя      номер_телефона
+# Ерасыл   12321313123
+# Человек2 23213142
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = [
+    ["ерасыл", ""],
+    ["человек2", "23213142"]
+]
+person1 = a[0]  # ["ерасыл", "12321313123"],
+d = {
+    "name": "yerassyl",
+    "phone_number": "12321313123"
+}
+print(d["name"], d["phone_number"])
