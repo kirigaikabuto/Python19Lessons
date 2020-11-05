d59060b11bffdc96c5e113bd646f4319f02f7369
YTleugazy@dar.kz <tleugazy98@gmail.com>
2020-11-05 19:40:10 +0600

    sdsd

---
+++ lesson14/2.py
@@ -0,0 +1,18 @@
+person1 = {
+    "first_name": "yerassyl",
+    "last_name": "tleugazy",
+    "age": 22,
+}
+person2 = {
+    "first_name": "person2",
+    "last_name": "person2",
+    "age": 50,
+}
+#code
+print(person1["age"])
+# print(person1["first_name"])
+# print(person1["last_name"])
+# person1["first_name"] = "name1"
+# person1["last_name"] = "last_name2"
+# print(person1["first_name"])
+# print(person1["last_name"])
