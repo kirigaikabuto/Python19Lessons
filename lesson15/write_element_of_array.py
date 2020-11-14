f5d279e20536b0b0715aa91f915dedc94614af26
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-14 12:03:10 +0600

    sdsd

---
+++ lesson15/write_element_of_array.py
@@ -0,0 +1,11 @@
+arr = [12, 23, 0, 123, ]
+n = len(arr)  # 4
+# range(4) -> [0,1,2,3]
+# for i in range(n):
+#     print(arr[i])
+    # arr[0]
+    # arr[1]
+    # arr[2]
+    # arr[3]
+for i in arr:
+    print(i)
