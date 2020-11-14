1ac7e0d46a4e339f2200d35723fd6bd70a4139d4
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-14 12:32:02 +0600

    sdsd

--- lesson15/FileWriterExample.py
+++ lesson15/FileWriterExample.py
@@ -1,8 +1,11 @@
 numbers = [1, 2, 3, 4, 5]
 file = open("out.txt", "w")
 # lines = ["1\n", "2\n", "3\n", "4\n", "5\n"]
-lines = []
 # code join
+newnumbers=[]
+for i in numbers:
+    newnumbers.append(str(i))
+print(newnumbers)
 
-file.writelines(lines)
+file.writelines()
 file.close()
