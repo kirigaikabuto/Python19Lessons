e77e2cd03d9cec33bc47890829dca386b2b41112
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-14 11:55:37 +0600

    sdsd

--- lesson15/add_element_to_array_with_loop.py
+++ lesson15/add_element_to_array_with_loop.py
@@ -1,13 +1,29 @@
 arr = []
-s = ""
-while s != "!":
+while True:
     s = input()
-    arr.append(s)
-arr.remove('!')
-numbers = []
-for i in arr:
-    numbers.append(int(i))
-print(numbers)
+    if s == "!":
+        break
+    number = int(s)
+    arr.append(number)
+print(arr)
+# step2
+# s = ""
+# while s != "!":
+#     s = input()
+#     if s != "!":
+#         number = int(s)
+#         arr.append(number)
+# print(arr)
+# step1
+# s = ""
+# while s != "!":
+#     s = input()
+#     arr.append(s)
+# arr.remove('!')
+# numbers = []
+# for i in arr:
+#     numbers.append(int(i))
+# print(numbers)
 # 3
 # 4
 # 5
