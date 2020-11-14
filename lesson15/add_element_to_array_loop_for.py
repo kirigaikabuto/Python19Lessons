e85a4ed0f6c1c4000962a41f3fc7e1fdd55c6ef7
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-14 12:00:36 +0600

    sdsd

---
+++ lesson15/add_element_to_array_loop_for.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = []
+# range(n)->[0..n]
+# range(1,n) -> [1
+# ..n]
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+print(arr)
+# 5
+# 123
+# 123
+# 12
+# 23
+# 23
