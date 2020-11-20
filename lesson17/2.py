41bc16165af9e837f117b6d2398f6c4b23cc1547
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-20 18:58:39 +0600

    sdsd

--- lesson17/2.py
+++ lesson17/2.py
@@ -1,10 +1,11 @@
 import json
 
+# read data
 filename = "users.json"
 file = open(filename, "r")
 dataJson = file.read()
 users = json.loads(dataJson)
-
+# create new data
 username = input("username:")
 password = input("password:")
 
@@ -13,6 +14,7 @@ user = {
     "password": password
 }
 users.append(user)
+# save data
 fileWrite = open(filename, "w")
 dataJson = json.dumps(users, indent=4)
 fileWrite.write(dataJson)
