bf44db17791a2a6f33cfcf850132c2e7967b0f35
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-20 19:30:49 +0600

    sdsd

--- lesson17/3.py
+++ lesson17/3.py
@@ -1,6 +1,35 @@
 import json
 
-file = open("testData.json", "r")
-data = file.read()
-jsonData = json.loads(data)
-print(jsonData)
+ingredient1 = {
+    "id": 1,
+    "name": "tomato",
+}
+ingredient2 = {
+    "id": 2,
+    "name": "cheese",
+}
+ingredient3 = {
+    "id": 3,
+    "name": "garlic"
+}
+ingredients1 = [ingredient1, ingredient2]
+pizza1 = {
+    "id": 1,
+    "title": "Primavera Pizza",
+    "price": 3.99,
+    "ingredients": ingredients1
+}
+ingredients2 = [ingredient1, ingredient3]
+pizza2 = {
+    "id": 2,
+    "title": "Vegetarian Pizza",
+    "price": 5.99,
+    "ingredients": ingredients2
+}
+pizzas = [pizza1, pizza1]
+# file write
+filename = "pizzas.json"
+file = open(filename, "w")
+dataJson = json.dumps(pizzas, indent=4)
+file.write(dataJson)
+file.close()
