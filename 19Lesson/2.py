da3e3bc835665a42a72e84b26b06bb5a05980b1e
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-27 19:15:44 +0600

    sdsd

--- 19Lesson/2.py
+++ 19Lesson/2.py
@@ -11,6 +11,14 @@ def getUserWithMaxSalary(data):
     return maxiUser
 
 
+def getUserWithMaxAge(data):
+    maxiUser = data[0]
+    for i in data:
+        if i["age"] > maxiUser["age"]:
+            maxiUser = i
+    return maxiUser
+
+
 user1 = {
     "name": "user1",
     "salary": 1230,
@@ -19,12 +27,15 @@ user1 = {
 user2 = {
     "name": "user2",
     "salary": 2230,
+    "age": 12,
 }
 user3 = {
     "name": "user3",
     "salary": 1000,
+    "age": 33,
 }
 users = [user1, user2, user3]
 printUsers(data=users)
 output = getUserWithMaxSalary(data=users)
-print(output)
+maxiAge = getUserWithMaxAge(data=users)
+print(maxiAge)
