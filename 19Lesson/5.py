014b5908f52dd8c5fc17c136ab958ecfaa15a9ba
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-27 19:36:25 +0600

    sdsd

--- 19Lesson/5.py
+++ 19Lesson/5.py
@@ -1,15 +1,11 @@
 import json
 from dictSupport import *
 
-
-
-
-
-users = readFromJsonFile("users.json")
+users = getDataFromJsonFile("users.json")
 user = {
     "name": "user4",
     "salary": 10000,
     "age": 123
 }
 users.append(user)
-saveToJsonFile("users.json", users)
+saveDataToJsonFile("users.json", users)
