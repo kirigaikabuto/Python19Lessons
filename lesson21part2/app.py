534e14e4197a669789c0148f8c4a1b4370a52dc7
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-05 12:40:46 +0600

    sdsd

--- lesson21part2/app.py
+++ lesson21part2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from livereload import Server
 
 app = Flask(__name__)
@@ -6,11 +6,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    firstName = "Yerassyl"
+    lastName = "TLeugazy"
+    return render_template("index.html", name=firstName, last_name=lastName)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route("/products/<name>")
+def products(name):
+    response = f"product {name}"
+    return response
 
 
 if __name__ == "__main__":
     app.debug = True
     server = Server(app.wsgi_app)
     server.serve()
-
