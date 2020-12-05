79d97d65e34821e70b488156c5549777c43b309b
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-05 11:51:31 +0600

    sdsd

--- lesson21/main.py
+++ lesson21/main.py
@@ -14,10 +14,22 @@ def home():
 
 
 @app.route("/about")
-def about():
+def about1():
     return "It is about page"
 
-#/contacts -> it is contacts page
-#/products -> it is products page
+
+@app.route("/about/<param1>")
+def about2(param1):
+    return "It is about page " + param1
+
+
+@app.route("/about/<param1>/<param2>")
+def about3(param1, param2):
+    return "It is about page " + param1 + " " + param2
+
+
+# /parametrs
+# /contacts -> it is contacts page
+# /products -> it is products page
 if __name__ == "__main__":
     app.run()
